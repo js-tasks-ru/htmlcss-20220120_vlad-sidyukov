3a37106b06551b1625dbc646f8218f86051d8c03
Vlad Sidyukov <vladsidyukov@yandex.ru>
2022-02-24 20:44:08 +0500

    Недочеты

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,10 +1,10 @@
 // Меню бургер
-const buttonMenu = document.querySelector('.header__button');
+const buttonMenu = document.querySelector('.header__toggler-mobile-menu');
 const menuBody = document.querySelector('.menu');
 if (buttonMenu) {
   buttonMenu.addEventListener("click", function (e) {
-    document.body.classList.toggle('_lock');
-    menuBody.classList.toggle('_active');
-    buttonMenu.classList.toggle('_active');
+    document.body.classList.toggle('content--lock');
+    menuBody.classList.toggle('menu--active');
+    buttonMenu.classList.toggle('header__toggler-mobile-menu--active');
   });
 }
