2e707996a1ef84f875dd3965547019cec0cead1a
Vlad Sidyukov <vladsidyukov@yandex.ru>
2022-02-26 17:12:20 +0500

    context-menu

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,10 +1,10 @@
 // Меню бургер
 const buttonMenu = document.querySelector('.header__toggler-mobile-menu');
-const menuBody = document.querySelector('.menu');
+const menuBody = document.querySelector('.context-menu');
 if (buttonMenu) {
   buttonMenu.addEventListener("click", function (e) {
     document.body.classList.toggle('content--lock');
-    menuBody.classList.toggle('menu--active');
+    menuBody.classList.toggle('context-menu--active');
     buttonMenu.classList.toggle('header__toggler-mobile-menu--active');
   });
 }
