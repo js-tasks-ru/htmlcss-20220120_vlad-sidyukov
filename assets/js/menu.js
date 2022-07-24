fe1eef376480a71296a4aabc313d2e859f521d64
Vlad Sidyukov <vladsidyukov@yandex.ru>
2022-07-24 22:57:55 +0500

    Готовый проект

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -4,7 +4,7 @@ const contextMenu = document.querySelector('.context-menu');
 function clickButtonMenuHandler() {
   if (contextMenuButton) {
     contextMenuButton.addEventListener("click", function () {
-      document.body.classList.toggle('content--lock');
+      document.documentElement.classList.toggle('content--lock');
       contextMenu.classList.toggle('context-menu--active');
       contextMenuButton.classList.toggle('header__toggler-mobile-menu--active');
     });
